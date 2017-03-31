06a70af14c78e56ee7c8397df427e30a8bd80b75
Bill Boughton <boughton.william@gmail.com>
2017-03-31 14:38:28 -0600

    get working

--- routes/tires.js
+++ routes/tires.js
@@ -1,8 +1,20 @@
-const router = require('express')Router()
+const router = require('express').Router()
 const knex = require('../db/knex.js')
 
+router.get('/', (req, res) => {
+  knex('tires')
+    .then(result => {
+      res.send(result)
+    })
+})
 
-
+router.get('/:id', (req, res) => {
+  var getId = req.params.id
+  knex('tires').where('id', getId)
+    .then(result => {
+      res.send(result)
+    })
+})
 
 
 
