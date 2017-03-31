9818444be16958b8b4c2288c6251cbedab9010be
Bill Boughton <boughton.william@gmail.com>
2017-03-31 14:46:05 -0600

    all routes working

--- routes/tires.js
+++ routes/tires.js
@@ -16,8 +16,35 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/new', (req, res) => {
+  var newBrand = req.body.brand
+  var newName = req.body.name
+  var newSize = req.body.size
+  knex('tires').insert({brand:newBrand, name:newName, size:newSize}).returning(['id', 'brand', 'name', 'size'])
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  var changeId = req.params.id
+  var changeBrand = req.body.brand
+  var changeName = req.body.name
+  var changeSize = req.body.size
+  knex('tires').where('id', changeId).update({name:changeName, brand:changeBrand, size:changeSize}).returning(['id', 'brand', 'name', 'size'])
+    .then(result => {
+      res.send(result)
+    })
+})
 
 
+router.delete('/:id', (req, res) => {
+  var dId = req.params.id
+  knex('tires').where('id', dId).del()
+    .then(result => {
+      res.send(200)
+    })
+})
 
 
 module.exports = router
